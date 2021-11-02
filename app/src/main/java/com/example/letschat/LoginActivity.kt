33be54acc947a1d2efef9d51ffd8ffd53c395c6d
Shady Kioko <kiokomusembi1@gmail.com>
2021-11-02 14:42:02 +0300

    firebase configs

--- app/src/main/java/com/example/letschat/LoginActivity.kt
+++ app/src/main/java/com/example/letschat/LoginActivity.kt
@@ -1,11 +1,31 @@
 package com.example.letschat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+
+        edtEmail = findViewById(R.id.log_in)
+        edtPassword = findViewById(R.id.password)
+        btnLogin = findViewById(R.id.btn_login)
+        btnSignUp = findViewById(R.id.btn_signup)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
