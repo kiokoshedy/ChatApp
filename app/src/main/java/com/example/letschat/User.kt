af5e8af0e0a23ef2cec3bde6e998a2ae522c9fc4
Shady Kioko <kiokomusembi1@gmail.com>
2021-11-02 17:18:10 +0300

    add users

--- app/src/main/java/com/example/letschat/User.kt
+++ app/src/main/java/com/example/letschat/User.kt
@@ -1,4 +1,19 @@
 package com.example.letschat
 
 class User {
+
+    var firstName: String? = null
+    var secondName: String? = null
+    var email: String? = null
+    var uid: String? = null
+
+    constructor() {}
+
+    constructor(firstName: String?, secondName: String?, email : String?) {
+
+        this.firstName = firstName
+        this.secondName = secondName
+        this.email = email
+        this.uid= uid
+    }
 }
