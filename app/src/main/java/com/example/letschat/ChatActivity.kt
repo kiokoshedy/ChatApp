bd8e2ef380a33c34b3ea4bf16394988109856445
Shady Kioko <kiokomusembi1@gmail.com>
2021-11-07 11:39:13 +0300

    changes

--- app/src/main/java/com/example/letschat/ChatActivity.kt
+++ app/src/main/java/com/example/letschat/ChatActivity.kt
@@ -1,5 +1,6 @@
 package com.example.letschat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,13 +13,22 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var chatRecyclerView: RecyclerView
     private lateinit var messageBox: EditText
     private lateinit var sendButton: ImageView
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageList: ArrayList<Message>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
+        val name = intent.getStringExtra("name")
+        val uid = intent.getStringExtra("uid")
+
+        supportActionBar?.title = name
+
         chatRecyclerView = findViewById(R.id.chatRecyclerView)
-        messageBox = findViewById(R.id.messageBox)
+        messageBox = findViewById(R.id.messageBox)s
         sendButton = findViewById(R.id.sendBtn)
+        messageList= ArrayList()
+        messageAdapter = MessageAdapter(this, messageList)
     }
 }
