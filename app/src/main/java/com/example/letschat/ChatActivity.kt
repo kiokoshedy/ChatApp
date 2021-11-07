50d534cfe7c4a8726bb3982d64473f0b6b7703f6
Shady Kioko <kiokomusembi1@gmail.com>
2021-11-07 11:23:49 +0300

    chat activity

--- app/src/main/java/com/example/letschat/ChatActivity.kt
+++ app/src/main/java/com/example/letschat/ChatActivity.kt
@@ -2,10 +2,23 @@ package com.example.letschat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var chatRecyclerView: RecyclerView
+    private lateinit var messageBox: EditText
+    private lateinit var sendButton: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        chatRecyclerView = findViewById(R.id.chatRecyclerView)
+        messageBox = findViewById(R.id.messageBox)
+        sendButton = findViewById(R.id.sendBtn)
     }
 }
